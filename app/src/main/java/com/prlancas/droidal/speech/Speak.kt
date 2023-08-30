67b8460f2a038670d7760dc176bad5b359471d5d
prlancasfd <paul.lancaster@fanduel.com>
2023-08-31 00:12:03 +0100

    Remove runBlocking from the main thread so it can initialise other things and make the application work again yay.

--- app/src/main/java/com/prlancas/droidal/speech/Speak.kt
+++ app/src/main/java/com/prlancas/droidal/speech/Speak.kt
@@ -3,28 +3,26 @@ package com.prlancas.droidal.speech
 import android.speech.tts.TextToSpeech
 import com.prlancas.droidal.event.EventBus
 import com.prlancas.droidal.event.events.Say
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import java.util.Locale
 
+@OptIn(DelicateCoroutinesApi::class)
 class Speak(val ttobj: TextToSpeech) {
-
+    private val scope = MainScope()
     init {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-//                launch {
-                    EventBus.subscribe<Say> {
-                        say(it.sentence)
-//                    }
-                }
+        scope.launch(newSingleThreadContext("MyOwnThread")) {
+            EventBus.subscribe<Say> {
+                say(it.sentence)
             }
         }
     }
 
     private fun say(sentence: String) {
-        ttobj.language = Locale.UK;
+        ttobj.language = Locale.UK
 
-        ttobj.speak(StringBuilder(sentence), TextToSpeech.QUEUE_ADD, null, sentence);
+        ttobj.speak(StringBuilder(sentence), TextToSpeech.QUEUE_ADD, null, sentence)
     }
 }
